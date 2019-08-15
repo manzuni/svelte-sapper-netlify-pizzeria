cbbf1ab2fee94790c535fc729350a95971598bbb
@mantonoiu <antonoiumadalin@gmail.com>
2019-08-15 05:22:26 +0000

    Fixed :) it now builds on Netlify

--- src/components/Nav.svelte
+++ src/components/Nav.svelte
@@ -464,18 +464,18 @@
 
 <nav class="navbar navbar-expand-lg navbar-dark ftco_navbar bg-dark ftco-navbar-light" id="ftco-navbar">
 	<div class="container">
-		<a class="navbar-brand" href="index.html"><span class="flaticon-pizza-1 mr-1"></span>Pizza<br><small>Delicous</small></a>
+		<a class="navbar-brand" href="."><span class="flaticon-pizza-1 mr-1"></span>Pizza<br><small>Delicous</small></a>
 		<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#ftco-nav" aria-controls="ftco-nav" aria-expanded="false" aria-label="Toggle navigation">
 		        <span class="oi oi-menu"></span> Menu
 		      </button>
 	      <div class="collapse navbar-collapse" id="ftco-nav">
 	        <ul class="navbar-nav ml-auto">
-	          <li class="nav-item active"><a href="index.html" class="nav-link">Home</a></li>
-	          <li class="nav-item"><a href="menu.html" class="nav-link">Menu</a></li>
-	          <li class="nav-item"><a href="services.html" class="nav-link">Services</a></li>
-	          <li class="nav-item"><a href="blog.html" class="nav-link">Blog</a></li>
-	          <li class="nav-item"><a href="about.html" class="nav-link">About</a></li>
-	          <li class="nav-item"><a href="contact.html" class="nav-link">Contact</a></li>
+	          <li class="nav-item active"><a href="." class="nav-link">Home</a></li>
+	          <li class="nav-item"><a href="." class="nav-link">Menu</a></li>
+	          <li class="nav-item"><a href="." class="nav-link">Services</a></li>
+	          <li class="nav-item"><a href="." class="nav-link">Blog</a></li>
+	          <li class="nav-item"><a href="." class="nav-link">About</a></li>
+	          <li class="nav-item"><a href="." class="nav-link">Contact</a></li>
 	        </ul>
 	      </div>
 		  </div>
